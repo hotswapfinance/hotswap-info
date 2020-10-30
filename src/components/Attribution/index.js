97d970df4075cf9583dd49ba9b81394911011ebe
KiWi Developer <kiwifinance.bsc@gmail.com>
2020-10-30 06:33:02 +0600

    hotswap-info

---
+++ src/components/Attribution/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Attribution = () => (
+  <p className="attribution">
+    <a href="https://github.com/" rel="noopener noreferrer" target="_blank">
+      Github
+    </a>{' '}
+    |{' '}
+    <a href="https://hotdog.cafe" rel="noopener noreferrer" target="_blank">
+      HOTDOG
+    </a>{' '}
+    |{' '}
+    <a href="/" rel="noopener noreferrer" target="_blank">
+      GIF
+    </a>
+  </p>
+)
+
+export default Attribution
